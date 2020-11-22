846fcf07a225f9695d25eaf11c7cdb6ec2ae3af8
aemarco <4aemarco@gmail.com>
2020-11-22 18:01:55 +0100

    Projektdateien hinzufügen.

---
+++ LockScreenTest/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Windows.System.UserProfile;
+
+namespace LockScreenTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            using var stream = File.OpenRead("test.jpg");
+            LockScreen.SetImageStreamAsync(stream.AsRandomAccessStream()).GetAwaiter().GetResult();
+        }
+    }
+}
